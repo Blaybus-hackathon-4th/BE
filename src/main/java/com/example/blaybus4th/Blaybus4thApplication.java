7b110bb271baae268a2ebcb97035fb8d1642dfed
kimyeongeuk <tklr0731@naver.com>
2026-02-02 21:54:41 +0900

    테스트 푸시

--- src/main/java/com/example/blaybus4th/Blaybus4thApplication.java
+++ src/main/java/com/example/blaybus4th/Blaybus4thApplication.java
@@ -3,6 +3,7 @@ package com.example.blaybus4th;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Blaybus4thApplication {
 
