16b125af1b1e4c814eb0c3addd141d8f349386f8
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-02 19:12:13 +0900

    Initial commit

---
+++ src/test/java/com/example/blaybus4th/Blaybus4thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blaybus4th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blaybus4thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
